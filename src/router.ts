d8a6e4420d42494459c941a626402bc734f8ac91
hugepizza <wangleilei950325@gmail.com>
2023-08-03 17:46:42 +0800

    fix repeat url

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,19 @@
-import { IRequest, Router, withContent } from 'itty-router';
+import { IRequest, Router, createCors, json, withContent } from 'itty-router';
+import { v4 as uuidv4 } from 'uuid';
+// create the CORS pair
+export const { preflight, corsify } = createCors({
+	methods: ['*'],
+	origins: ['*'],
+	headers: {
+		'*': '*',
+	},
+});
 
 const errors = {
 	'404': () => new Response('Not Found.', { status: 404 }),
 	'401': () => new Response('No Auth.', { status: 401 }),
 	'400': () => new Response('Bad Request.', { status: 400 }),
+	'403': () => new Response('Refuse Request.', { status: 403 }),
 };
 
 const router = Router();
@@ -12,33 +22,105 @@ interface essay {
 	code: string;
 	content: string;
 	updatedAt: string;
+	view: number;
 }
 
-router.get('/api/c/:id', async (request, kv) => {
-	const id = request.params['id'];
-	const essayStr = await (<KVNamespace>kv).get(id);
+interface essayCreate {
+	code?: string;
+	content: string;
+	url?: string;
+	updatedAt: string;
+}
+
+export interface essayUpdate {
+	content: string;
+	code?: string;
+	newUrl?: string;
+	newCode?: string;
+}
+
+router.all('*', preflight);
+
+router.get('/api/c/:url', async (request, kv) => {
+	const url = request.params['url'];
+	const essayStr = await (<KVNamespace>kv).get(url);
 	if (!essayStr) {
 		return errors[404]();
 	}
 	const fetchedEssay = JSON.parse(essayStr) as essay;
-	const code = request.query['code'];
-	if (fetchedEssay.code != code) {
-		return errors[401]();
-	}
-	return new Response(fetchedEssay.content);
+
+	fetchedEssay.view = fetchedEssay.view + 1;
+	await (<KVNamespace>kv).put(url, JSON.stringify(fetchedEssay));
+	return json(fetchedEssay);
 });
 
-router.post('/api/c/:id', async (request, kv) => {
-	const id = request.params['id']
+router.post('/api/c', async (request, kv) => {
+	const kvDB = <KVNamespace>kv;
+	let returnUrl = '';
 	try {
-		const body = await request.json<essay>();
-		await (<KVNamespace>kv).put(id, JSON.stringify(body));
+		const requestEssay = await request.json<essayCreate>();
+		if (requestEssay.content === '') {
+			return errors[400]();
+		}
+		if (requestEssay.url) {
+			const essayStr = await kvDB.get(requestEssay.url);
+			if (essayStr) {
+				return errors[403]();
+			}
+		}
+		requestEssay.updatedAt = new Date().toISOString();
+		if (!requestEssay.url) {
+			requestEssay.url = crypto.randomUUID();
+		}
+
+		// new update
+		requestEssay.updatedAt = new Date().toISOString();
+		await kvDB.put(requestEssay.url, JSON.stringify(requestEssay));
+		returnUrl = requestEssay.url;
 	} catch (err) {
 		console.log(err);
 		return errors[400]();
 	}
 
-	return new Response(id);
+	return new Response(returnUrl);
+});
+
+router.post('/api/c/:url', async (request, kv) => {
+	const kvDB = <KVNamespace>kv;
+	const oldUrl = request.params['url'];
+	let newUrl = oldUrl;
+	try {
+		const requestEssay = await request.json<essayUpdate>();
+		if (requestEssay.content === '') {
+			return errors[400]();
+		}
+		const essayStr = await kvDB.get(oldUrl);
+		if (!essayStr) {
+			return errors[404]();
+		}
+		const fetchedEssay = JSON.parse(essayStr) as essay;
+		if (!fetchedEssay.code || fetchedEssay.code != requestEssay.code) {
+			return errors[401]();
+		}
+		fetchedEssay.updatedAt = new Date().toISOString();
+		fetchedEssay.content = requestEssay.content;
+		if (requestEssay.newCode) {
+			fetchedEssay.code = requestEssay.newCode;
+		}
+		if (requestEssay.newUrl && oldUrl != requestEssay.newUrl) {
+			if (await kvDB.get(newUrl)) {
+				return errors[401]();
+			}
+			await kvDB.delete(oldUrl);
+			newUrl = requestEssay.newUrl;
+		}
+		await kvDB.put(newUrl, JSON.stringify(fetchedEssay));
+	} catch (err) {
+		console.log(err);
+		return errors[400]();
+	}
+
+	return new Response(newUrl);
 });
 
 router.all('*', () => errors[404]());
--- src/worker.ts
+++ src/worker.ts
@@ -8,21 +8,16 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import apiRouter from './router';
-
-export interface Env {
-	kv: KVNamespace;
-}
+import apiRouter, { corsify } from './router';
 // Export a default object containing event handlers
 export default {
 	// The fetch handler is invoked when this worker receives a HTTP(S) request
 	// and should return a Response (optionally wrapped in a Promise)
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
-		env.kv.put('sb', '傻卵');
 		const url = new URL(request.url);
 		if (url.pathname.startsWith('/api/')) {
-			return await apiRouter.handle(request, env.kv);
+			return corsify(await apiRouter.handle(request, env.kv));
 		}
 		return new Response(`<h1>Hava a good day.</h1>`, { headers: { 'Content-Type': 'text/html' } });
 	},
